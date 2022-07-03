0f5da33cac8ecf3c5b5b70110094d777ea1035ee
SHRUTI SINGH <shruticourses16@gmail.com>
2022-07-03 15:47:00 +0530

    Update 16.cpp

--- 16.cpp
+++ 16.cpp
@@ -1,15 +1,14 @@
 #include<iostream>
 #include<bits/stdc++.h>
-#include <boost/multiprecision/cpp_int.hpp>
 using namespace std;
 
 int main()
 {
     int n;
     cin>>n;
-    int128_t ans = pow(2,n);
-    int128_t temp = ans;
-    int128_t sum = 0;
+    long long int ans = pow(2,n);
+    long long int temp = ans;
+    long long int sum = 0;
     while(ans>0){
         sum = sum + ans%10;
         ans = ans/10;
